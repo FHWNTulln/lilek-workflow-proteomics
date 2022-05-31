a1b4cfabc333fce52867cd8888cb41b4791b69de
david <lilek@tulln.fhwn.ac.at>
2022-05-31 16:01:28 +0200

    update grep proteingroups

--- grep_proteinGroups.sh
+++ grep_proteinGroups.sh
@@ -2,7 +2,7 @@
 
 (1) login to server e.g. via putty
 (2) cd /proj/proteomics/<project directory>/<results>
-(3) find . -maxdepth 5 -name "proteinGroups.txt" -exec bash -c 'for x; do x=${x#./}; cp -u "$x" "/proj/proteomics/tmp/${x//\//_}"; done' _ {} +
+(3) find . -maxdepth 5 -name "proteinGroups.txt" -exec bash -c 'for x; do x=${x#./}; cp -u "$x" "/proj/proteomics/protein_groups_2gether/${x//\//_}"; done' _ {} +
 (4) files are stored here /proj/proteomics/tmp/
 (5) use e.g. WINSCP to search for the protein group you are interested in and download it
 -> search optins winscp https://winscp.net/eng/docs/ui_find?ver=5.19.1&lang=0407&utm_source=winscp&utm_medium=app&utm_campaign=5.19.1
