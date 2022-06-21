122580a630a64f020d1e89a91724cd9512f3daf9
davidlilek <lilek@fhwn.ac.at>
2022-06-21 09:19:52 +0200

    finished script download from ftp server

--- download_ftp.py
+++ download_ftp.py
@@ -1,28 +1,26 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+
 """
-Created on Mon Apr 11 11:11:06 2022
+script for downloading raw-files from FTP server
+
+to download files run the following command in a bash terminal
+
+python download_ftp.py | wget -c -i- 
+
+flag -c: try to continue abborted download
+flag -i-: read the URL from stdin
 
 @author: david
 """
-# https://www.educative.io/edpresso/what-is-the-html-parser-in-python
+
 import requests
 import re
-# def extract(url):
-#     # Get webpage
-#     response = requests.get(url)
-#     # Get page source
-#     #print(response.text)
-#     result = re.findall('<a[^>]*>', response.text)
-#     print(result)
-    
-url = "http://ftp.ebi.ac.uk/pride-archive/2014/09/PXD000279/"
-
-#extract(url)
-
-
 from html.parser import HTMLParser
 
+# define url where data should be downloaded    
+url = "http://ftp.ebi.ac.uk/pride-archive/2014/09/PXD000279/"
+
+# create class HTML parser to get links from FTP server
 class MyHTMLParser(HTMLParser):
     """This child class defines a method to handle starttags.
     See https://docs.python.org/3/library/html.parser.html
@@ -32,35 +30,15 @@ class MyHTMLParser(HTMLParser):
         #results = []
         if tag != 'a':  # we're only interested in <a> tags
             return
-        #print(attrs)
-        #ls.append(attrs[0][1])
-        #print(attrs[0][1])
         results.append(attrs[0][1])
-        #print(ls)
-        #return ls
-        #print(type(attrs[0][1]))
-    # def get_links(self, attr):
-    #     count = 0 
-    #     print(type(attr))
-    #     for i in attr:
-    #         count += 1
-    #         #print(f"http://ftp.ebi.ac.uk/pride-archive/2014/09/PXD000279/{attrs}")
-    #     print(count)
 
-        
-# def extract(url):    
-#     response = requests.get(url)
-#     parser = MyHTMLParser()
-#     parser.feed(response.text)
-#     #return parser.feed(response.text)
-#     return parser
+# create list with results and get urls         
 results = []   
- 
 response = requests.get(url)
 parser = MyHTMLParser()
 parser.feed(response.text)
 
-
+# print links for raw files
 base = "http://ftp.ebi.ac.uk/pride-archive/2014/09/PXD000279/"
 for i in results:
     if "raw" in i.lower():
