6ec8f0b4d00d504796c74bdf68d1dcd40810c0f4
davidlilek <lilek@tulln.fhwn.ac.at>
2022-08-17 20:04:38 +0200

    updates R

--- run-maxquant.sh
+++ run-maxquant.sh
@@ -184,10 +184,10 @@ if [ $R ==  "yes" ] && [[ $runs == 1 ]]; then
     path="/proj/proteomics/$projname/results/results\_run$runs\_$filename"
     Rscript ./bin/run-R.R $path
     #move file
-    cp -rf ./$projname/results/results\_run$runs\_$filename/post-processing.html ./$projname/evaluation/post-processing.html
+    cp -rf ./$projname/results/results\_run$runs\_$filename/post-processing.html ./$projname/evaluation/post-processing-$filename.html
     rm -rf ./$projname/results/results\_run$runs\_$filename/post-processing.html
     echo "post-processing R sucessful" | tee -a log.txt
-    echo "result file: ./$projname/evaluation/post-processing.html" | tee -a log.txt
+    echo "result file: ./$projname/evaluation/post-processing-$filename.html" | tee -a log.txt
 fi
 
 ###############################
